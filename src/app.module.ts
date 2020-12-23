a4cfc3d65230795050117cb351aeeb491a0681cb
rattapon.simsri0@gmail.com <rattapon.simsri0@gmail.com>
2020-12-23 15:11:59 +0700

    create login

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { LoginModule } from './login/login.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule, LoginModule],
 })
 export class AppModule {}
